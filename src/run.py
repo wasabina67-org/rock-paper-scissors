bee5dcc7ba3f51e45ed13b2a44d7e52ccf3dad22
wasabina67 <wasabina67@gmail.com>
2024-07-26 08:27:05 +0000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,6 +1,12 @@
 def main():
     options = ["Rock", "Paper", "Scissors"]
 
+    # Player's input
+    player_choice = input("Choose Rock, Paper, or Scissors: ")
+    if player_choice not in options:
+        print("Invalid input.")
+        return
+
 
 if __name__ == "__main__":
     main()
