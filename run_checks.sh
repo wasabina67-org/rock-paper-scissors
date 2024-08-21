fc0241d2fa0b662cc202d3e870dc9e66f3d659df
wasabina67 <wasabina67@gmail.com>
2024-08-21 09:28:03 +0000

    Update run_checks.sh

--- run_checks.sh
+++ run_checks.sh
@@ -4,3 +4,8 @@ isort src/run.py src/utils.py
 black src/run.py src/utils.py
 flake8 src/run.py src/utils.py
 mypy src/run.py src/utils.py
+
+isort tests/test_utils.py
+black tests/test_utils.py
+flake8 tests/test_utils.py
+mypy tests/test_utils.py
